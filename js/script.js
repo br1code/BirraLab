bc1354037f2875bc4d2215185aff9ff7378b46d6
Br1 <brunogiovagnoli@gmail.com>
2018-03-25 22:49:51 -0300

    Change style, video settings and remove comments

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-var myvids = [];
-var valid = [];
+var myvids = ["videos/maceracion.mp4", "videos/hervor.mp4", 
+              "videos/enfriado.mp4", "videos/fermentacion.mp4", 
+              "videos/maduracion.mp4", "videos/embotellamiento.mp4"];
 var myvid = document.getElementById('myvideo');
 var activeVideo = 0;
 
@@ -7,61 +8,26 @@ function play(){
   myvid.addEventListener('ended', addVideos);
 }
 
-function addVideos(e){
-  if(activeVideo+1 === myvids.length){
+function addVideos(e) {
+  if (activeVideo + 1 === myvids.length) {
     myvid.removeEventListener('ended', addVideos)
   }
   // update the video source and play
   myvid.src = myvids[activeVideo];
   myvid.play();
+  highlight();
   // update the new active video index
   activeVideo = (++activeVideo);
 }
 
-function macerar(){
-  myvids.push("videos/maceracion.mp4");
-  valid.push(1);
-  appendInstruction("Macerar");
-}
-function hervir(){ 
-  myvids.push("videos/hervor.mp4");
-  valid.push(2);
-  appendInstruction("Hervir");
-}
-function enfriar(){
-  myvids.push("videos/enfriado.mp4");
-  valid.push(3);
-  appendInstruction("Enfriar");
-}
-function fermentar(){
-  myvids.push("videos/fermentacion.mp4");
-  valid.push(4);
-  appendInstruction("Fermentar");
-}
-function madurar(){
-  myvids.push("videos/maduracion.mp4");
-  valid.push(5);
-  appendInstruction("Madurar");
-}
-function embotellar(){
-  myvids.push("videos/embotellamiento.mp4");
-  valid.push(6);
-  appendInstruction("Embotellar");
+function startInstr() {
+  setTimeout(() => {
+    myvid.play();
+  }, 500);
 }
 
-function check(){
-  for(var i=0;i<6;++i){
-    if(valid[i] != i+1){
-      myvids.push("videos/wrong.mp4");
-      break;
-    }
-  }
-}
-
-function appendInstruction(text) {
-    var node = document.createElement("LI");
-    var textnode = document.createTextNode(text);
-    node.appendChild(textnode);
-    document.getElementById("pasoAPaso").appendChild(node);
+function highlight() {
+  $("#pasoAPaso li").removeClass("highlight");
+  $(`#${activeVideo}`).addClass("highlight");
 }
 
